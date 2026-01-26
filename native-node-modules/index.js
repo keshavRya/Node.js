b84f99d9dd2aed43348e3868209b1fce07d23ff8
keshavRya <keshavaryaiit@gmail.com>
2026-01-27 00:04:05 +0530

    fs.writefile overwrites data

--- native-node-modules/index.js
+++ native-node-modules/index.js
@@ -1,9 +1,20 @@
-console.log("learning node.js");
-
 const fs = require("fs");
 
 
-fs.writeFile("./sample.txt", "this is keshav arya. i repeat this is keshav arya", (err) => {
+// fs.writeFile("./sample.txt", "this is keshav arya. i repeat this is keshav arya", (err) => {
+//     if (err) throw err;
+
+//     fs.readFile("./sample.txt", "utf-8", (err, data) =>{
+//         if (err) throw err;
+
+//         console.log(data);
+//     });
+// });
+
+
+
+
+fs.writeFile("./sample.txt", "today is my 25th birthday!\nits now or never buddy", (err) => {
     if (err) throw err;
 
     fs.readFile("./sample.txt", "utf-8", (err, data) =>{
